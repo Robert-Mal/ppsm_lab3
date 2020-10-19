12a8a5b3cc8befde3345add37d6e3f98e1fa729e
Robert-Mal <33180@student.pwsztar.edu.pl>
2020-10-19 15:01:08 +0200

    second

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Generuj(View view) {
+        TextView joke = findViewById(R.id.joke);
+        String[] tab = {"What an electrician is doing on stage... builds tension",
+                "What is the name of the line produced in the USA... trampoline",
+                "Why the blonde throws the clock through the window... because he watches time go by",
+                "What is the soldier's favorite fruit... pomegranate",
+                "Why is it hot in the corner... because it's 90 degrees"};
+        Random rand = new Random();
+        int rand_number = rand.nextInt(5);
+        joke.setText(tab[rand_number]);
     }
 }
